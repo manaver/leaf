75f0dbbc81634fb37a2314c006a5c7755f122d24
eludadev <111eluda111@gmail.com>
2022-07-07 09:33:58 +0100

    Add firebase

--- src/main.js
+++ src/main.js
@@ -6,6 +6,28 @@ import router from './router'
 
 import './index.css'
 
+
+// =================== Initialize firebase ==========================
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGufgwHgIZStd_QffOk6GEMMcBgNtOqWk",
+  authDomain: "leaf-mediagroup.firebaseapp.com",
+  projectId: "leaf-mediagroup",
+  storageBucket: "leaf-mediagroup.appspot.com",
+  messagingSenderId: "547575279711",
+  appId: "1:547575279711:web:e809d70e0a6520e8a4ebea"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// ===================   Initialize Vue    ==========================
+
 const app = createApp(App)
 
 app.use(createPinia())
